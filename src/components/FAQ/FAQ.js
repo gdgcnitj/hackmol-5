6054c757bc4e70fcf9160d4dcda41b590e6256f5
chahatkesh <ckesharwani4@gmail.com>
2024-01-10 20:37:34 +0530

    done faq

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,71 +1,147 @@
 import React, { useState } from "react";
-
+import Image from "next/image";
 // *******TODO: Update FAQ Questions*******
 
-let qa = [
-    {
-        ques: "What is a Hackathon and who are eligible to participate in HackMol4.0 ?",
-        ans: "A Hackathon is an intense event that brings together designers and developers along with industry experts to identify problems and create software solutions, usually within 24-48 hours. If you're a student developer, we'd love to have you at HackMol4.0! Whether that's a undergrad, or graduate, we believe you can get something out of the event."
-    },
-    {
-        ques: "What can be the size of team?",
-        ans: `A team can consist of 1-4 members.\n Pro Tip : Aim to have a mix of people with both design and developer skills. Individual entries are also allowed.`
-    },
-    {
-        ques: "What if I don't have a team or idea ?",
-        ans: "Don't worry, most people don't! We'll have team formation and ideation events geared towards helping you find people to work with. It's perfectly normal to go around to people, tell them you're looking for a team, and ask if you can help out. You can also show yourself off at #teamup on our Discord Server."
-    },
-    {
-        ques: "When we will receive goodies?",
-        ans: "Prizes and goodies are provided by sponsors of the hackathon, hence they are responsible for the delivery of the items."
-    }
-]
 
+
+// let FAQ = function () {
+ 
+//     const FAQItem = ({ question, answer }) => {
+//       const [isOpen, setIsOpen] = useState(false);
+    
+//       const toggleAnswer = () => {
+//         setIsOpen(!isOpen);
+//       };
+//     return (
+      
+//         <>
+//      <div className=" w-full h-full relative">
+//      <div className="banner-img inset-0 w-full flex flex-row items-center bg-black opacity-50 h-32 md:h-64 " style={{ backgroundImage: "url('/images/FAQs.png')"  } }>
+//      <span className="text-white flex items-center  m-auto  text-7xl md:text-9xl font-Minecrafter font-bold leading-10">FAQ</span>
+//      </div>
+// </div>
+// <div className="w-full h-full px-10 pb-12 bg-stone-900 justify-start items-center ">
+// <div className="flex-col w-full items-start gap-14">
+//   <div className="flex flex-col items-start gap-6 ">
+//     <div className="w-full py-3.5 border-b-2 border-white flex items-center justify-between gap-5">
+//       <div className="text-white w-3/4 text-2xl font-bold font-['Space Grotesk'] leading-9">Lorem ipsum dolor sit.</div>
+//       <div className="text-white text-5xl font-light font-['Inter'] text-right">+</div>
+//     </div>
+//     <div className="w-full py-3.5 border-b-2 border-white flex items-center justify-between  gap-5">
+//       <div className="text-white w-3/4 text-2xl font-bold font-['Space Grotesk'] leading-9">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor?</div>
+//       <div className="text-white text-5xl font-light font-['Inter']">+</div>
+//     </div>
+//     <div className="w-full py-3.5 border-b-2 border-white flex items-center justify-between gap-5">
+//       <div className="text-white w-3/4 text-2xl font-bold font-['Space Grotesk'] leading-9">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</div>
+//       <div className="text-white text-5xl font-light  font-['Inter'] mr-[0]">+</div>
+//     </div>
+//     <div className="w-full self-stretch h-full py-2.5 border-b border-white flex-col items-start gap-5">
+//       <div className="w-full gap-5 flex items-start  justify-between">
+//         <div className="text-white w-3/4 text-2xl font-bold font-['Space Grotesk'] leading-9">Lorem ipsum dolor sit amet ffhfg.</div>
+//         <div className="text-white text-5xl font-light font-['Inter'] cursor-pointer" onClick={toggleAnswer}>
+//         <span>{isOpen ? '-' : '+'}</span>
+//         </div>
+//       </div>
+//       <div className="w-full text-white text-xl font-normal font-['Space Grotesk'] leading-loose">
+//         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+//       </div>  
+//     </div>
+//   </div>
+// </div>
+
+//     <div className="px-4 py-2 border-2 border-white justify-center items-center gap-2.5 inline-flex">
+//       <div className="text-center text-white text-2xl font-bold font-['Space Grotesk'] leading-9">LOAD MORE +</div>
+//     </div>
+//   </div>
+
+
+
+//      {/* <img className="banner-right-img inset-0 bg-black opacity-50" src="/images/FAQs.png" /> */}
+   
+     
+//         </>
+//     )
+// }}
 let FAQ = function () {
-    const [index, setIndex] = useState(() => {
-        return 0;
-    })
+  const [showAll, setShowAll] = useState(false);
+  const [qa, setQA] = useState([
+    {
+      ques: "What is a Hackathon and who are eligible to participate in HackMol4.0 ?",
+      ans: "A Hackathon is an intense event that brings together designers and developers along with industry experts to identify problems and create software solutions, usually within 24-48 hours. If you're a student developer, we'd love to have you at HackMol4.0! Whether that's a undergrad, or graduate, we believe you can get something out of the event.",
+    },
+    {
+      ques: "What can be the size of team?",
+      ans: `A team can consist of 1-4 members.\n Pro Tip : Aim to have a mix of people with both design and developer skills. Individual entries are also allowed.`,
+    },
+    {
+      ques: "What if I don't have a team or idea ?",
+      ans: "Don't worry, most people don't! We'll have team formation and ideation events geared towards helping you find people to work with. It's perfectly normal to go around to people, tell them you're looking for a team, and ask if you can help out. You can also show yourself off at #teamup on our Discord Server.",
+    },
+    {
+      ques: "When will we receive goodies?",
+      ans: "Prizes and goodies are provided by sponsors of the hackathon, hence they are responsible for the delivery of the items.",
+    },
+  ]);
 
-    let FAQPositionerIncrease = () => {
-        if (index == qa.length - 1) {
-            setIndex(0);
-        }
-        else {
-            setIndex(prevIndex => prevIndex + 1);
-        }
-    }
+  const FAQItem = ({ question, answer }) => {
+    const [isOpen, setIsOpen] = useState(false);
 
-    let FAQPositionerDecrease = () => {
-        if (index == 0) {
-            setIndex(qa.length - 1);
-        }
-        else {
-            setIndex(prevIndex => prevIndex - 1)
-        }
-    }
+    const toggleAnswer = () => {
+      setIsOpen(!isOpen);
+    };
+
     return (
-        <div className="bg-orange flex flex-col  justify-center items-center  w-full py-10 md:py-20 ">
-            <h1 className="text-black  text-3xl  md:text-4xl  font-extrabold items-center pb-4 text-center ">FAQs</h1>
-            <div className="bg-yellow shadow-3xl  min-h-[545px] mb-10 p-6 md:min-h-[350px] md:px-10 md:pt-10 md:pb-16  border w-11/12 sm:w-8/12 border-black  flex flex-col rounded-lg justify-items-center items-center">
-                <h2 className="p-4 text-2xl text-center"><b>{qa[index].ques}</b></h2>
-                <text className="text-center self-start text-lg whitespace-pre-line	" >{qa[index].ans}</text>
-            </div>
-            <div className="bg-blue w-[60%] rounded-lg   h-4 relative border shadow-3xl border-black">
-                <div className="w-[98px] h-[60px] absolute -top-6 left-[50%] -translate-x-[50%] rounded-full flex bg-blue justify-center gap-3  items-center border border-black">
-                    <button onClick={() => { FAQPositionerDecrease() }}>
-                        <svg className="h-[25px] w-[25px] hover:mr-2" width="23" height="26" viewBox="0 0 23 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M0.5 13L23 0.00961876V25.9904L0.5 13Z" fill="#070606" />
-                        </svg>
-
-                    </button>
-                    <button className="h-[25px] w-[25px]  hover:ml-2" onClick={() => { FAQPositionerIncrease() }}><svg width="23" height="26" viewBox="0 0 23 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M22.5 13L0 25.9904L0 0.00961876L22.5 13Z" fill="#070606" />
-                    </svg>
-                    </button>
-                </div>
-            </div>
+      <div className="w-full h-full px-10 py-10 bg-stone-900 justify-start items-center ">
+       <div className="flex-col w-full items-start gap-14">
+         <div className="flex flex-col items-start gap-6 ">
+      <div className="w-full self-stretch h-full py-2.5 border-b border-white flex-col items-start gap-5">
+      <div className="w-full py-3.5  border-white flex items-center justify-between  gap-5">  
+        <div className="text-white w-3/4 text-2xl font-bold font-['Space Grotesk'] leading-9">{question}</div>
+        <div className="text-white text-5xl font-light font-['Inter'] cursor-pointer" onClick={toggleAnswer}>
+          <span>{isOpen ? '-' : '+'}</span>  
+          </div>
         </div>
-    )
-}
+        {isOpen &&  <div className="w-full gap-5 flex items-start  justify-between">
+      <div className="w-full text-white text-xl font-normal font-['Space Grotesk'] leading-loose">
+             {answer}    
+             </div>  </div>}
+      </div>
+      </div>
+      </div>
+      </div>
+    );
+  };
 
-export default FAQ
+  const renderQuestions = () => {
+    const questionsToShow = showAll ? qa : qa.slice(0, 3);
+    return questionsToShow.map((item, index) => <FAQItem key={index} question={item.ques} answer={item.ans} />);
+  };
+
+  const handleLoadMore = () => {
+    setShowAll(true);
+  };
+
+  return (
+    <>
+      <div className="w-full h-full relative">
+        <div className="banner-img inset-0 w-full flex flex-row items-center bg-black opacity-50 h-32 md:h-64 " style={{ backgroundImage: "url('/images/FAQs.png')" }}>
+          <span className="text-white flex items-center  m-auto  text-7xl md:text-9xl font-Minecrafter font-bold leading-10">FAQ</span>
+        </div>
+      </div>
+      <div className="w-full h-full px-10 pb-12 bg-stone-900 justify-start items-center ">
+        <div className="flex-col w-full items-start gap-14">{renderQuestions()}</div>
+        {!showAll && (
+          <div className=" w-full px-4 py-2  m-auto  justify-center items-center gap-2.5 inline-flex cursor-pointer" onClick={handleLoadMore}>
+            <div className="border-2 "><div className=" m-3 text-center flex justify-center items-center text-white text-2xl font-bold font-['Space Grotesk'] leading-9">LOAD MORE +</div></div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+
+
+
+export default FAQ;
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
       <Judges />
 
       <Testimonies/>
-      {/* <FAQ></FAQ> */}
+      <FAQ />
       <Footer></Footer>
       <FooterLinks></FooterLinks>
     </div>
