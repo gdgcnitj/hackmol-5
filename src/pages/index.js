fa4e2ab01cca87cd6b8c0716a08509da74ee6c11
chahatkesh <ckesharwani4@gmail.com>
2024-01-10 23:14:03 +0530

    previous year

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,14 @@
-
 // import Schedule from '../components/Timeline/Schedule'
 // import Script from 'next/script'
 
-import PrizeSection from '../components/PrizeSection'
+import PrizeSection from "../components/PrizeSection";
+import Testimonies from "../components/Testimonies";
 
 export default function Home() {
-
   return (
-    <div className=''>
-
-      hi
-          <PrizeSection/>
+    <div className="">
+      <PrizeSection />
+      <Testimonies />
     </div>
-  )
+  );
 }
