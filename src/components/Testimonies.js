31f4016058b484ac8c6da02d731c7d4645bba386
chahatkesh <ckesharwani4@gmail.com>
2024-01-12 11:14:21 +0530

    updated

--- src/components/Testimonies.js
+++ src/components/Testimonies.js
@@ -1,24 +1,27 @@
 import t1 from "../../public/images/Reviews/tweetbird2.svg";
 import t2 from "../../public/images/Reviews/tweetbird3.svg";
+import style from "../styles/testimonie.module.css";
 
 function Testimonies() {
   return (
-    <div className="flex flex-col text-center bg-[#1F1F1F] p-0 space-y-4  px-2vw h-fit">
-      <div className="text-center text-white text-5xl my-12 ">
+    <div className="flex flex-col text-center bg-[#1F1F1F] p-0 space-y-4  px-2vw h-fit mt-[-2px] pb-[2px]">
+      <div className={` m-auto py-2  ${style.heading1} text-sm`}>
         PREVIOUS YEAR REVIEWS
       </div>
       <div className=" flex relative ">
-        <div className=" relative md:top-[1vw]  top-[15vw]  z-[4]">
+        <div className=" relative md:top-[1vw]  top-[12vw]  z-[4]">
           <img src={t1.src} alt="#" className="z-[4]" />
         </div>
 
-        <div className="bg-white text-center  border-4 border-pink-400 lg:w-[80vw] md:h-[26vw] lg:h-[20vw] w-[100vw]  h-[45vw] relative lg:right-[20vw] left-[-20vw] right-0  lg:pl-[11vw] pl-[5vw] pr-[1vw] py-[3vw]">
+        <div className="bg-white text-center  border-4 border-pink-400 lg:w-[80vw] md:h-[26vw] lg:h-[20vw] w-[100vw]  w-fit h-fit relative lg:right-[20vw] left-[-20vw] right-0  md:pl-[10vw] pl-[5vw] pr-[1vw] py-[2.5vw]  ">
           <div>
             <div className="flex  sm:font-bold sm:text-xl  text-lg ">
               <div className="bg-[#1F1F1F] w-[30px] h-[30px] mr-4"></div>
               <h1>Aditya</h1>
             </div>
-            <div className="text-left sm:text-base  text-[2.2vw]">
+            <div
+              className={`sm:text-left text-start sm:text-base  text-[10px] ${style.font}`}
+            >
               <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute top-[-1.2vw] right-[-1.3vw]"></div>
               <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute top-[-1.2vw] right-[2vw]"></div>
               <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute top-[2.1vw] right-[-1.3vw]"></div>
@@ -32,19 +35,21 @@ function Testimonies() {
         </div>
       </div>
       <div className="flex relative">
-        <div className="bg-white  text-center  border-4 border-pink-400  lg:w-[80vw] w-[100vw]  lg:h-[20vw] md:h-[26vw]  h-[45vw] relative lg:left-[20vw] right-[-20vw] lg:pr-[11vw] pr-[5vw] pl-[1vw] py-[3vw] ">
+        <div className="bg-white  text-center  border-4 border-pink-400  lg:w-[80vw] w-fit h-fit  lg:h-[20vw] md:h-[26vw]   relative lg:left-[20vw] right-[-20vw] md:pr-[10vw] pr-[5vw] pl-[1vw] py-[2.5vw]  ">
           <div>
             <div className="flex text-xl sm:font-bold sm:text-xl  text-lg justify-end ">
               <h1>Nikhil</h1>
-              <div className="bg-[#1F1F1F] w-[30px] h-[30px] ml-4"></div>
+              <div className="bg-[#1F1F1F] w-[30px] h-[30px] ml-4 "></div>
             </div>
-            <div className="text-right  sm:text-base text-[2.2vw]">
-              <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute bottom-[-1.2vw] left-[-1.2vw]"></div>
+            <div
+              className={`sm:text-right text-end sm:text-base  text-[10px] ${style.font}`}
+            >
+              <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute bottom-[-1.2vw] left-[-1.3vw]"></div>
               <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute top-[-1.2vw] right-[-1.3vw]"></div>
               <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute top-[-1.2vw] right-[2vw]"></div>
               <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute top-[2.1vw] right-[-1.3vw]"></div>
-              <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute bottom-[-1.2vw] left-[2.1vw]"></div>
-              <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute bottom-[2.1vw] left-[-1.2vw]"></div>
+              <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute bottom-[-1.2vw] left-[2.0vw]"></div>
+              <div className="bg-[#1F1F1F] w-[3.5vw] h-[3.5vw] absolute bottom-[2.1vw] left-[-1.3vw]"></div>
               Participating in the GDSC hackathon at NITJ was an incredible
               experience.The event,filled with innovative energy.The
               well-organized event,from registration to problem statements.
@@ -53,7 +58,7 @@ function Testimonies() {
             </div>
           </div>
         </div>
-        <div className="relative  md:top-[1vw] top-[14vw] right-0">
+        <div className="relative  md:top-[1vw] top-[12vw] right-0 overflowing-component ">
           <img src={t2.src} className="" alt="#" />
         </div>
       </div>
