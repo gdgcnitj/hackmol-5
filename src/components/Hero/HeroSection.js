ea9895a0c46594cf9cd8e3a941ce13678391a335
chahatkesh <ckesharwani4@gmail.com>
2024-01-17 03:47:51 +0530

    Responsive behaviour added

--- src/components/Hero/HeroSection.js
+++ src/components/Hero/HeroSection.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import heroSection from "../../../public/images/HeroText.svg";
+import heroText from "../../../public/images/HeroText.svg";
 import linkArrow from "../../../public/images/LinkArrow.svg";
 import regButton from "../../../public/images/RegisterButton.svg";
 import regButtonHover from "../../../public/images/RegisterButtonHover.svg";
@@ -15,13 +15,13 @@ function HeroSection() {
       <div className="flex flex-col justify-center items-center relative top-[6rem]">
         <div className="flex relative py-8">
           <img
-            src={heroSection.src}
+            src={heroText.src}
             alt="#"
             style={{ mixBlendMode: "hard-light" }}
-            className="w-70"
+            className="w-[22rem] sm:w-[40rem] lg:w-[60rem]"
           />
         </div>
-        <div className="font-Minecraft text-6xl font-normal flex relative py-8 flex-col justify-center items-center">
+        <div className="font-Minecraft text-2xl sm:text-4xl lg:text-6xl font-normal flex relative py-8 flex-col justify-center items-center">
           <div className="flex relative">THINK OUTSIDE THE BOX</div>
           <div className="flex relative">HACK INSIDE IT</div>
         </div>
