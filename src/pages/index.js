302b2e79c660ceef6269067d9356426966d748af
chahatkesh <ckesharwani4@gmail.com>
2024-01-19 09:53:40 +0530

    timelinesection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 // import Script from 'next/script'
 import Testimonies from "../components/Testimonies";
+import Timeline from "../components/Timeline";
 import Rules from "../components/Rules/Rules";
 import FAQ from "../components/FAQ/FAQ";
 import PrizeSection from "../components/PrizeSection";
@@ -8,9 +9,10 @@ export default function Home() {
   return (
     <div>
       <PrizeSection />
+      <Timeline />
       <Testimonies />
       <Rules />
-      <FAQ/>
+      <FAQ />
     </div>
   );
 }
